7de9be073807618cc01dfe29c28832c296d8119b
Samantha Phipps <sammi.phipps@gmail.com>
2019-09-25 20:42:40 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,10 +1,20 @@
 def help
+  puts "I accept the following commands:"
+  puts "- help : displays this help message"
+  puts "- list : displays a list of songs you can play"
+  puts "- play : lets you choose a song to play"
+  puts "- exit : exits this program"
 end 
 
 def play
 end 
 
-def list
+def list(song_array)
+  song_array.
 end 
 
 def exit_jukebox
+end 
+
+def run(song_array)
+end 
